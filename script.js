c7278571520b7c72136fbd179eca06fc203ba1b3
Gikonyo-Otieno <gikonyootieno@gmail.com>
2022-11-20 20:42:10 +0300

    Add JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-/*const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);*/
+function hashNoHistory(link){
+    var a = document.createElement("a");
+    a.href = link.href;
+    location.replace(a.href);
+    return false;
+  }
 
 
 
